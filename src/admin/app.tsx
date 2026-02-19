7b88e69ae4820202c98a6e587d0c925b4ed32581
akuczewski <kuczewski.a@gmail.com>
2026-02-19 19:09:51 +0100

    feat: register ingredient-lookup custom field globally in admin/app.tsx as fallback

---
+++ src/admin/app.tsx
@@ -0,0 +1,34 @@
+
+import { Search } from '@strapi/icons';
+// @ts-ignore
+// @ts-ignore
+import IngredientLookup from '../plugins/ingredient-lookup/admin/src/components/IngredientLookup/index';
+
+export default {
+    config: {
+        locales: ['pl'],
+    },
+    bootstrap(app: any) {
+        console.log('[APP] Registering global ingredient-lookup custom field...');
+
+        app.customFields.register({
+            name: 'ingredient-lookup',
+            pluginId: 'ingredient-lookup', // Hardcoded pluginId to match usage in schema
+            type: 'string',
+            intlLabel: {
+                id: 'ingredient-lookup.label',
+                defaultMessage: 'Ingredient Lookup (Firebase)',
+            },
+            intlDescription: {
+                id: 'ingredient-lookup.description',
+                defaultMessage: 'Search and select an ingredient from the global database',
+            },
+            icon: Search,
+            components: {
+                Input: async () => IngredientLookup,
+            },
+        });
+
+        console.log('[APP] Global custom field registered.');
+    },
+};
